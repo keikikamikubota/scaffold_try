e222250db7fc2c4c0dfc1d19b5ad97a5d934f69b
keiki <kkeki0410@gmail.com>
2023-07-15 07:16:16 +0900

    7/14初期

---
+++ db/migrate/20230713002907_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.string :g
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
